39ac1502e93af6ad8a0d335d0c0fa8632dba7651
deepakilango <deepak.ilango@gmail.com>
2016-04-11 16:50:11 -0500

    First checkin of Terminal map application

--- app.js
+++ app.js
@@ -1,29 +1,151 @@
-/*eslint-env node*/
+var app = require('express')()
+  , express = require('express') 
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , AssetProvider= require ('./assetprovider').AssetProvider
+  
+  , mongo = require("mongodb")
+  , QueryCommand = mongo.QueryCommand
+  , Cursor = mongo.Cursor
+  , Collection = mongo.Collection;
+  
+var assetProvider= new AssetProvider('localhost', 27017);
+  
+//var local = new Db('local',new Server('localhost',27017,{safe:false},{auto_reconnect:true}, {}));
+  
+server.listen(3030);
+  
+//Specify the views folder
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+//app.set("views",__dirname);
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+//View engine is Jade
 
-// create a new express server
-var app = express();
+//app.set("view engine", "jade");
+app.use(express.static(__dirname +'/public'));
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+app.get('/', function (req, res) {
+  console.log('in app.get() method');
+  res.sendfile(__dirname + '/saml.html');
+  //res.render("index");
 });
+  
+io.on('connection', function (socket) {
+console.log('connected');
+  assetProvider.getCollection(function(error,asset_collection){
+  //console.log(asset_collection);
+  /*var latestCursor =  asset_collection.find().sort({$natural: -1}).limit(1);
+  console.log(latestCursor);
+  latestCursor.nextObject(function(err, latest) {
+    if (latest) {
+	var obj = [];
+      obj._id = {$gt: latest._id};
+    }
+    var options = {
+      tailable: true,
+      await_data: true,
+      numberOfRetries: -1
+    };
+    var stream = asset_collection.find(obj, options).sort({$natural: -1}).stream();
+    stream.on('data',function(item){console.log(item);});
+    });*/
+	
+    readAndSend(socket,asset_collection);
+  /*asset_collection.find({}, {"tailable": 1, "sort": [["$natural", 1]]}, function(err, cursor) {
+		cursor.intervalEach(300, function(err, item) { 
+		console.log("ho");
+			if(item != null) {
+				console.log(item);
+				}
+			});
+		});*/
+  });
+	
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+ socket.emit("jinga",{lala:'hu'});
+ socket.on('jinga', function (blah){
+ console.log("jinga");
+ console.log(blah); 
+  });
+ socket.emit("change");
+ socket.on("start",function(data){
+		console.log(data);
+		assetProvider.findAsset(1,function(error,result){
+		socket.emit("first",result);});
+		assetProvider.findAsset(2,function(error,result){
+		socket.emit("second",result);});
+		assetProvider.findAsset(3,function(error,result){
+		socket.emit("third",result);});
+		assetProvider.findAsset(4,function(error,result){
+		socket.emit("fourth",result);});
+ });
+
+  socket.on("onmousedown", function (data) {
+    var abc=data;
+    console.log("id = " + data.lock);
+    assetProvider.findAsset(data.lock,function(error,result){
+			console.log(result);
+                        console.log(result.status);
+			if (result.status=='unlock')
+				{console.log("Go Ahead");
+				socket.emit("ok",data);
+				var newStat = "lock";
+				assetProvider.updateStat(data.lock,newStat,function(error,newStat){console.log("updated");});}
+			else
+				console.log("Shut the **** up.");
+			});    
+  });
+  socket.on("onmousemove", function (data) {
+    var xyz=data;
+    console.log("left:" +xyz.left+" , top:" +xyz.top);
+    assetProvider.updateLoc(xyz.id,xyz,function(error,xyz){
+				socket.broadcast.emit("new",data);});
+  });
+  socket.on("onmouseup", function (data) {
+    console.log("id = " +data.unlock);
+    var newStat = "unlock";
+  assetProvider.updateStat(data.unlock,newStat,function(error,newStat){console.log("updated");});
+  socket.broadcast.emit("mouseup",data);
+  });
+});
+
+Cursor.prototype.intervalEach = function(interval, callback) {
+    var self = this;
+    if (!callback) {
+		throw new Error("callback is mandatory");
+    }
+
+    if(this.state != Cursor.CLOSED) {
+		setTimeout(function(){
+			self.nextObject(function(err, item) {        
+				if(err != null) return callback(err, null);
+				if(item != null) {
+					callback(null, item);
+					self.intervalEach(interval, callback);
+				} else {
+					self.state = Cursor.CLOSED;
+					callback(err, null);
+				}
+
+				item = null;
+			});
+		}, interval);
+    } else {
+		callback(new Error("Cursor is closed"), null);
+    }
+};
+
+  
+function readAndSend (socket, collection) {
+    console.log('readAndSend method');
+    collection.find({}, {"tailable": 1, "sort": [["$natural", 1]]}, function(err, cursor) {
+		cursor.intervalEach(5, function(err, item) { 
+			if(item != null ) {
+				console.log(item);
+			}
+		});
+	});
+};
